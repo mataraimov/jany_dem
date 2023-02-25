140780eb5fecded7ca0931f989c582d45bb5c494
mataraimov <minuradil@gmail.com>
2023-02-25 12:41:13 +0600

    initial commit

---
+++ jany_dem/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jany_dem project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jany_dem.settings')
+
+application = get_wsgi_application()
